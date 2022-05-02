f801999be50f3dbedb0ad02d12efec22ab8a6231
Duncan Chen <ago30425@gmail.com>
2022-05-03 00:06:27 +0800

    Implement remove and insert methods for linked list

--- linked-list-good-taste/main.c
+++ linked-list-good-taste/main.c
@@ -28,8 +28,56 @@ void list_append(node_t **head, int val)
     *indirect = new_node;
 }
 
+void list_remove(node_t **head, int val)
+{
+    node_t **indirect;
+
+    if (!head)
+        return;
+
+    indirect = head;
+    while (*indirect && (*indirect)->val != val) {
+        indirect = &((*indirect)->next);
+    }
+
+    if (*indirect) {
+        *indirect = (*indirect)->next;
+    }
+}
+
+/* Insert a new val before the specific val. */
+void list_insert(node_t **head, int new_val, int val)
+{
+    node_t *new_node, **indirect;
+
+    if (!head)
+        return;
+
+    new_node = (node_t *)malloc(sizeof(node_t));
+    if (!new_node)
+        return;
+
+    new_node->val = new_val;
+    new_node->next = NULL;
+
+    indirect = head;
+    while (*indirect && (*indirect)->val != val) {
+        indirect = &((*indirect)->next);
+    }
+
+    if (*indirect) {
+        new_node->next = *indirect;
+        *indirect = new_node;
+    }
+}
+
 void list_print(node_t *head)
 {
+    if (!head) {
+        printf("No any nodes\n");
+        return;
+    }
+
     while (head) {
         printf("%d ", head->val);
 
@@ -50,11 +98,45 @@ int main(void)
     if (!head)
         return -1;
 
-    /* Append numbers */
+    printf("* Append 1, 2, 3\n");
     list_append(head, 1);
     list_append(head, 2);
     list_append(head, 3);
+    list_print(*head);
 
+    printf("* Remove 2\n");
+    list_remove(head, 2);
+    list_print(*head);
+    printf("* Remove 3\n");
+    list_remove(head, 3);
+    list_print(*head);
+    printf("* Remove 4\n");
+    list_remove(head, 4);
+    list_print(*head);
+    printf("* Remove 1\n");
+    list_remove(head, 1);
+    list_print(*head);
+
+    printf("* Append 1, 2, 3\n");
+    list_append(head, 1);
+    list_append(head, 2);
+    list_append(head, 3);
+    list_print(*head);
+
+    printf("* Insert 5 before 3\n");
+    list_insert(head, 5, 3);
+    list_print(*head);
+
+    printf("* Insert 6 before 1\n");
+    list_insert(head, 6, 1);
+    list_print(*head);
+
+    printf("* Insert 7 before 5\n");
+    list_insert(head, 7, 5);
+    list_print(*head);
+
+    printf("* Insert 8 before 9\n");
+    list_insert(head, 8, 9);
     list_print(*head);
 
     free(head);
